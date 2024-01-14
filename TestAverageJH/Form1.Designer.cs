356b4412369cef483a3c25caba1a9d3aebefdbd4
jennoBombs <96265765+jennoBombs@users.noreply.github.com>
2024-01-14 05:55:44 -0800

    Added TestAverage to repository

---
+++ TestAverageJH/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TestAverageJH
+{
+    partial class testAverageJH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test1PromptLabelJH = new System.Windows.Forms.Label();
+            this.test2PromptLabelJH = new System.Windows.Forms.Label();
+            this.test3PromptLabelJH = new System.Windows.Forms.Label();
+            this.outputDescriptionLabelJH = new System.Windows.Forms.Label();
+            this.test1TextBoxJH = new System.Windows.Forms.TextBox();
+            this.test2TextBoxJH = new System.Windows.Forms.TextBox();
+            this.test3TextBoxJH = new System.Windows.Forms.TextBox();
+            this.averageLabelJH = new System.Windows.Forms.Label();
+            this.CalculateButtonJH = new System.Windows.Forms.Button();
+            this.ClearButtonJH = new System.Windows.Forms.Button();
+            this.ExitButtonJH = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test1PromptLabelJH
+            // 
+            this.test1PromptLabelJH.AutoSize = true;
+            this.test1PromptLabelJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test1PromptLabelJH.Location = new System.Drawing.Point(68, 55);
+            this.test1PromptLabelJH.Name = "test1PromptLabelJH";
+            this.test1PromptLabelJH.Size = new System.Drawing.Size(53, 18);
+            this.test1PromptLabelJH.TabIndex = 0;
+            this.test1PromptLabelJH.Text = "Test 1:";
+            // 
+            // test2PromptLabelJH
+            // 
+            this.test2PromptLabelJH.AutoSize = true;
+            this.test2PromptLabelJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test2PromptLabelJH.Location = new System.Drawing.Point(68, 89);
+            this.test2PromptLabelJH.Name = "test2PromptLabelJH";
+            this.test2PromptLabelJH.Size = new System.Drawing.Size(53, 18);
+            this.test2PromptLabelJH.TabIndex = 1;
+            this.test2PromptLabelJH.Text = "Test 2:";
+            // 
+            // test3PromptLabelJH
+            // 
+            this.test3PromptLabelJH.AutoSize = true;
+            this.test3PromptLabelJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test3PromptLabelJH.Location = new System.Drawing.Point(68, 122);
+            this.test3PromptLabelJH.Name = "test3PromptLabelJH";
+            this.test3PromptLabelJH.Size = new System.Drawing.Size(53, 18);
+            this.test3PromptLabelJH.TabIndex = 2;
+            this.test3PromptLabelJH.Text = "Test 3:";
+            // 
+            // outputDescriptionLabelJH
+            // 
+            this.outputDescriptionLabelJH.AutoSize = true;
+            this.outputDescriptionLabelJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputDescriptionLabelJH.Location = new System.Drawing.Point(31, 161);
+            this.outputDescriptionLabelJH.Name = "outputDescriptionLabelJH";
+            this.outputDescriptionLabelJH.Size = new System.Drawing.Size(142, 18);
+            this.outputDescriptionLabelJH.TabIndex = 3;
+            this.outputDescriptionLabelJH.Text = "Average Test Score:";
+            // 
+            // test1TextBoxJH
+            // 
+            this.test1TextBoxJH.Location = new System.Drawing.Point(156, 51);
+            this.test1TextBoxJH.Name = "test1TextBoxJH";
+            this.test1TextBoxJH.Size = new System.Drawing.Size(100, 22);
+            this.test1TextBoxJH.TabIndex = 4;
+            // 
+            // test2TextBoxJH
+            // 
+            this.test2TextBoxJH.Location = new System.Drawing.Point(156, 89);
+            this.test2TextBoxJH.Name = "test2TextBoxJH";
+            this.test2TextBoxJH.Size = new System.Drawing.Size(100, 22);
+            this.test2TextBoxJH.TabIndex = 5;
+            // 
+            // test3TextBoxJH
+            // 
+            this.test3TextBoxJH.Location = new System.Drawing.Point(156, 122);
+            this.test3TextBoxJH.Name = "test3TextBoxJH";
+            this.test3TextBoxJH.Size = new System.Drawing.Size(100, 22);
+            this.test3TextBoxJH.TabIndex = 6;
+            // 
+            // averageLabelJH
+            // 
+            this.averageLabelJH.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.averageLabelJH.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.averageLabelJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageLabelJH.Location = new System.Drawing.Point(193, 161);
+            this.averageLabelJH.Name = "averageLabelJH";
+            this.averageLabelJH.Size = new System.Drawing.Size(100, 23);
+            this.averageLabelJH.TabIndex = 7;
+            // 
+            // CalculateButtonJH
+            // 
+            this.CalculateButtonJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateButtonJH.Location = new System.Drawing.Point(25, 208);
+            this.CalculateButtonJH.Name = "CalculateButtonJH";
+            this.CalculateButtonJH.Size = new System.Drawing.Size(106, 63);
+            this.CalculateButtonJH.TabIndex = 8;
+            this.CalculateButtonJH.Text = "Calculate Average";
+            this.CalculateButtonJH.UseVisualStyleBackColor = true;
+            this.CalculateButtonJH.Click += new System.EventHandler(this.calculateButtonJH_Click);
+            // 
+            // ClearButtonJH
+            // 
+            this.ClearButtonJH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearButtonJH.Location = new System.Drawing.Point(150, 207);
+            this.ClearButtonJH.Name = "ClearButtonJH";
+            this.ClearButtonJH.Size = new System.Drawing.Size(106, 64);
+            this.ClearButtonJH.TabIndex = 9;
+            this.ClearButtonJH.Text = "Clear";
+            this.ClearButtonJH.UseVisualStyleBackColor = true;
+            this.ClearButtonJH.Click += new System.EventHandler(this.clearButtonJH_Click);
+            // 
+            // ExitButtonJH
+            // 
+            this.ExitButtonJH.Location = new System.Drawing.Point(272, 207);
+            this.ExitButtonJH.Name = "ExitButtonJH";
+            this.ExitButtonJH.Size = new System.Drawing.Size(106, 63);
+            this.ExitButtonJH.TabIndex = 10;
+            this.ExitButtonJH.Text = "Exit";
+            this.ExitButtonJH.UseVisualStyleBackColor = true;
+            this.ExitButtonJH.Click += new System.EventHandler(this.exitButtonJH_Click);
+            // 
+            // testAverageJH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(401, 344);
+            this.Controls.Add(this.ExitButtonJH);
+            this.Controls.Add(this.ClearButtonJH);
+            this.Controls.Add(this.CalculateButtonJH);
+            this.Controls.Add(this.averageLabelJH);
+            this.Controls.Add(this.test3TextBoxJH);
+            this.Controls.Add(this.test2TextBoxJH);
+            this.Controls.Add(this.test1TextBoxJH);
+            this.Controls.Add(this.outputDescriptionLabelJH);
+            this.Controls.Add(this.test3PromptLabelJH);
+            this.Controls.Add(this.test2PromptLabelJH);
+            this.Controls.Add(this.test1PromptLabelJH);
+            this.Name = "testAverageJH";
+            this.Text = "Test Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label test1PromptLabelJH;
+        private System.Windows.Forms.Label test2PromptLabelJH;
+        private System.Windows.Forms.Label test3PromptLabelJH;
+        private System.Windows.Forms.Label outputDescriptionLabelJH;
+        private System.Windows.Forms.TextBox test1TextBoxJH;
+        private System.Windows.Forms.TextBox test2TextBoxJH;
+        private System.Windows.Forms.TextBox test3TextBoxJH;
+        private System.Windows.Forms.Label averageLabelJH;
+        private System.Windows.Forms.Button CalculateButtonJH;
+        private System.Windows.Forms.Button ClearButtonJH;
+        private System.Windows.Forms.Button ExitButtonJH;
+    }
+}
+
---
+++ TestAverageJH/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAverageJH
+{
+    public partial class testAverageJH : Form
+    {
+        public testAverageJH()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButtonJH_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double test1;   // To hold test score #1
+                double test2;   // To hold test score #2
+                double test3;   // To hold test score #3
+                double average; // To hold the average test score
+
+                // Get the three test scores.
+                test1 = double.Parse(test1TextBoxJH.Text);
+                test2 = double.Parse(test2TextBoxJH.Text);
+                test3 = double.Parse(test3TextBoxJH.Text);
+
+                // Calculate the average test score.
+                average = (test1 + test2 + test3) / 3.0;
+
+                // Display the average test score, with
+                // the output rounded to 1 decimal point.
+                averageLabelJH.Text = average.ToString("n1");
+            }
+            catch (Exception ex)
+            {
+                // Display the default error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButtonJH_Click(object sender, EventArgs e)
+        {
+            // Clear the input and output controls.
+            test1TextBoxJH.Text = "";
+            test2TextBoxJH.Text = "";
+            test3TextBoxJH.Text = "";
+            averageLabelJH.Text = "";
+        }
+
+        private void exitButtonJH_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
